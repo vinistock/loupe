39246087dc0c11b27d09182525ab78a14cada11b
Vinicius Stock <stock@hey.com>
2021-04-22 19:53:25 -0400

    Remove unused test class inside parallel_test_helper

--- suite/test/fake/parallel_test_helper.rb
+++ suite/test/fake/parallel_test_helper.rb
@@ -9,25 +9,3 @@ module ActiveSupport
     parallelize(workers: :number_of_processors)
   end
 end
-
-class ParallelTest < ActiveSupport::TestCase
-  def setup
-    @number = 0
-  end
-
-  def test_number
-    assert_equal 0, @number
-  end
-
-  def test_failing_test
-    refute_equal 0, 0 % 500
-  end
-
-  def test_the_truth
-    assert true
-  end
-
-  def test_the_truth2
-    assert true
-  end
-end
