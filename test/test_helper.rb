0aa1e829302debfb85930895a17690c79aa5cde2
Vinicius Stock <stock@hey.com>
2021-07-27 20:12:35 -0400

    Fix test_helper typo

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "Loupe"
+require "loupe"
 require "minitest/autorun"
 require "purdytest"
 require "mocha/minitest"
